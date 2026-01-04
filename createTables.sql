67d5be73d37b5afe1b2a418c4cb4585f38cb646a
Anjali Veerapaneni <anjaliveerapaneni28@gmail.com>
2026-01-04 14:58:12 +0530

    Updated text file

--- createTables.sql
+++ createTables.sql
@@ -1,8 +1,10 @@
-
+--Create a database.
+CREATE DATABASE fullstack;
 USE fullstack;
 
---create student table
-	CREATE TABLE Students (
+
+-- Student (VTU Number, Name, Email, Phone, Department) 
+	CREATE TABLE Student (
     VTU_Number VARCHAR(20) PRIMARY KEY,
     Name VARCHAR(100),
     Email VARCHAR(100),
@@ -10,16 +12,12 @@ USE fullstack;
     Department VARCHAR(50)
 );
 
---create course table
+--Course (Course Code, Course Name, Faculty Id, Student ID, Faculty Email) 
     CREATE TABLE Course (
     Course_Code VARCHAR(10) PRIMARY KEY,
     Course_Name VARCHAR(100),
     Faculty_Id INT,
     Student_ID VARCHAR(20),
     Faculty_Email VARCHAR(100),
-    FOREIGN KEY (Student_ID) REFERENCES Students(VTU_Number)
+    FOREIGN KEY (Student_ID) REFERENCES Student(VTU_Number)
 );
-
-
-
-
