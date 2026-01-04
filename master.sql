bce54d8778f6ef44a26abc06ca5f3e484e837c9d
Anjali Veerapaneni <anjaliveerapaneni28@gmail.com>
2026-01-04 15:32:40 +0530

    Updated text files

--- master.sql
+++ master.sql
@@ -1,5 +1,5 @@
 -- Active: 1767517213029@@127.0.0.1@3306@fullstack
-SOURCE 01_createtables.sql;
-SOURCE 02_DataInsertion.sql;
-SOURCE 03_joins.sql;
-SOURCE 04_selectclauses.sql;
+SOURCE createtables.sql;
+SOURCE DataInsertion.sql;
+SOURCE joins.sql;
+SOURCE selectclauses.sql;
